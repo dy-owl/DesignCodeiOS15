9b1614a9ce758ed87c113c8a2bcf9a26f3c04e9b
Dmytro Yantsybaiev <d.yantsybaiev@icloud.com>
2021-12-31 16:27:18 +0200

    Initial Commit

---
+++ DesignCodeiOS15/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesignCodeiOS15
+//
+//  Created by Dmytro Yantsybaiev on 31.12.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DesignCodeiOS15/DesignCodeiOS15App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeiOS15App.swift
+//  DesignCodeiOS15
+//
+//  Created by Dmytro Yantsybaiev on 31.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeiOS15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
