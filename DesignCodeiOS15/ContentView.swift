3849ca46f65d07b40ef8130287f645e519e7e240
Dmytro Yantsybaiev <d.yantsybaiev@icloud.com>
2021-12-31 18:33:41 +0200

    - configure Card Hero with 3 Texts and Image("Logo 2")

--- DesignCodeiOS15/ContentView.swift
+++ DesignCodeiOS15/ContentView.swift
@@ -9,8 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading, spacing: 8.0) {
+            Spacer()
+            Image("Logo 2")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 26.0, height: 26.0)
+                .cornerRadius(20.0)
+            Text("SwiftUI for iOS 15")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            Text("20 sections - 3 hours")
+                .font(.footnote)
+                .fontWeight(.semibold)
+                .textCase(.uppercase)
+            Text("Build an iOS app for iOS 15 with custom layouts, animations and ...")
+                .font(.footnote)
+                .fontWeight(.regular)
+                .lineLimit(2)
+        }
+        .padding(.all, 20.0)
+        .frame(height: 350.0)
+        .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color("Background")/*@END_MENU_TOKEN@*/)
+        .cornerRadius(30.0)
+        .shadow(radius: 20)
     }
 }
 
