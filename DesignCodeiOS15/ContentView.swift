3dea253af15316b4ea976ff306372bb37bd930db
Dmytro Yantsybaiev <d.yantsybaiev@icloud.com>
2022-01-01 00:59:39 +0200

    Shape, Stroke and Blend Mode

--- DesignCodeiOS15/ContentView.swift
+++ DesignCodeiOS15/ContentView.swift
@@ -15,7 +15,9 @@ struct ContentView: View {
                 .resizable(resizingMode: .stretch)
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 26.0, height: 26.0)
-                .cornerRadius(20.0)
+                .cornerRadius(20)
+                .padding(9)
+                .background(.regularMaterial, in: RoundedRectangle(cornerRadius: 20, style: .continuous))
             Text("SwiftUI for iOS 15")
                 .font(.largeTitle)
                 .fontWeight(.bold)
@@ -35,9 +37,13 @@ struct ContentView: View {
         .padding(.all, 20.0)
         .padding(.vertical, 20)
         .frame(height: 350.0)
-        .background(.regularMaterial)
-        .cornerRadius(30.0)
+        .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 30, style: .continuous))
         .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
+        .overlay(
+            RoundedRectangle(cornerRadius: 30, style: .continuous)
+                .stroke(.linearGradient(colors: [.white.opacity(0.3), .black.opacity(0.1)], startPoint: .top, endPoint: .bottom))
+                .blendMode(.overlay)
+        )
         .padding(.horizontal, 20)
         .background(Image("Blob 1").offset(x: 250, y: -100))
         .overlay(
@@ -52,7 +58,11 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
-            .preferredColorScheme(.light)
+        Group {
+            ContentView()
+                .preferredColorScheme(.light)
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
     }
 }
